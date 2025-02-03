da277d27410e95b8ad0d78c4cc8b065afaa8b580
Benjamin Lambright <bwlambright@gmail.com>
2025-02-03 10:43:30 -0500

    Add files via upload

--- ocr-eval.py
+++ ocr-eval.py
@@ -11,7 +11,8 @@ import chardet
 SAMPLE_SIZE = 19
 
 
-def process_gold(gold_path, tesseract_five_path):
+def process_gold(gold_path, tesseract_five_path) -> tuple[list[str], list[str]]:
+    """Reformatting the gold files so it can be compared to OCR outputs, as well as the order"""
     with open(gold_path, "r") as file:
         golds = file.read()
         # print(f"correct_order: {golds}")
@@ -26,19 +27,8 @@ def process_gold(gold_path, tesseract_five_path):
 
     return gold_output, correct_order
 
-# def sort_tesseract_five_outputs(gold_path, tesseract_five_path):
-#     with open(gold_path, "r") as file:
-#         golds = file.read()
-    
-#     # sorting the outputs so they are in the same order as the gold_path
-#     print(f"example: {golds[0][0]}")
-#     correct_order = [os.path.join(tesseract_five_path, golds[i][0][:-3] + "txt") 
-#                         for i in list(range(0, SAMPLE_SIZE))]
-
-#     print(f"output_paths: {correct_order}")
-#     return correct_order
-
-def calc_doctr(image_path):
+def calc_doctr(image_path) -> str:
+    """calculating the doctr result for an image, matching the style of the gold, so newlines are with \n"""
     single_img_doc = DocumentFile.from_images(image_path)
 
     result = model(single_img_doc)
@@ -54,50 +44,34 @@ def calc_doctr(image_path):
     return "\n".join(sentences)
 
 
-def calc_tesseract(image_path):
+def calc_tesseract(image_path) -> str:
+    """tesseract4 output for a single image"""
     tesseract_output = pytesseract.image_to_string(Image.open(image_path))
 
     return tesseract_output
 
-def process_tesseract_five(gold_path, tesseract_five_path):
-    # output_paths = os.listdir(image_dir)
-    # output_paths = [os.path.join(image_dir, output_path) for output_path in output_paths]
-    # # print(output_paths)
-    # output_paths.remove('tesseract_five_outputs/.DS_Store')
-
-    # output_paths = sort_tesseract_five_outputs(gold_path, tesseract_five_path)
-
+def process_tesseract_five(correct_order) -> list[str]:
+    """tesseract5 output extraction for all images"""
     outputs = []
     for output in correct_order:
-        with open(output, "r", encoding="utf-8", errors="ignore") as f:
+        with open(output, "r", encoding="utf-8", errors="ignore") as f:  # we had to clarify the output because the tesseract outputs were not all
             output_lines = [line.strip() for line in f]
             output_str = "\n".join(output_lines)
             outputs.append(output_str)
 
-        # with open(output, "rb") as f:
-        #     raw_data = f.read()
-        #     result = chardet.detect(raw_data)
-        #     # print(f"Detected encoding: {result['encoding']} for {output}")
-
-        #     # the encoding is ascii unless otherwise if it's empty or ds store
-        #     if str(result['encoding']) == "ascii":
-        #         with open(output, "r", encoding="ascii") as f:
-
-        #     output_lines = [line.strip() for line in f]
-        #     output_str = "\n".join(output_lines)
-        #     outputs.append(output_str)
-
-    # print(f"outputs ok? {outputs}")
     return outputs
 
 
-def calc_cer(gold_output, doctr_output, tesseract_output):
+def calc_cer(gold_output, doctr_output, tesseract_output) -> tuple[int, int]:
+    """Calculating the cer for doctr and tesseract4"""
     doctr_results = cer(gold_output, doctr_output)
     tesseract_results = cer(gold_output, tesseract_output)
 
     return doctr_results, tesseract_results
 
 def main_tests(images, gold_outputs):
+    """running and logging the doctr and tesseract4 tests"""
+
     # run the doctr tests
     print("loading doctr...")
     model = ocr_predictor(pretrained=True)
@@ -114,6 +88,15 @@ def main_tests(images, gold_outputs):
     print(f"doctr_results: {doctr_results}")
     print(f"tesseract4_results: {tesseract_results}")
 
+def tesseract_five_test(correct_order, tesseract_five_path):
+    """running and logging the tesseract5 test"""
+
+    print("processing tesseract5")
+    tesseract_outputs = process_tesseract_five(correct_order, tesseract_five_path)
+    print(f"outputs: {tesseract_outputs}")
+    print(f"gold outputs: {gold_outputs}")
+    print(f"tesseract5_results: {cer(gold_outputs, tesseract_outputs)}")
+
 
 
 if __name__ == "__main__":
@@ -129,13 +112,10 @@ if __name__ == "__main__":
     images = [os.path.join(image_dir, image) for image in images]
 
     # main tests
-    # main_tests(images, gold_outputs)
+    main_tests(images, gold_outputs)
 
     # calculating tesseract5
-    tesseract_outputs = process_tesseract_five(correct_order, tesseract_five_path)
-    print(f"outputs: {tesseract_outputs}")
-    print(f"gold outputs: {gold_outputs}")
-    print(f"tesseract5_results: {cer(gold_outputs, tesseract_outputs)}")
+    tesseract_five_test(correct_order, tesseract_five_path)
 
 
 
